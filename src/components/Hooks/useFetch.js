7e5b672d89f27604e3b27e5f9163e92bd7d5b04c
Jéssica Neri <jess.neri94@gmail.com>
2021-06-22 16:31:11 -0300

    Feat: CreateLogin and useFetch (not finished)

---
+++ src/components/Hooks/useFetch.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export default function useFetch() {
+  const [data, setData] = useState(null);
+  return <div></div>;
+}
--- src/components/Hooks/useForm.js
+++ src/components/Hooks/useForm.js
@@ -6,6 +6,11 @@ const validation = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido",
   },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
+    message:
+      "A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Mínimo de 8 caracteres.",
+  },
 };
 
 export default function useForm(type) {
