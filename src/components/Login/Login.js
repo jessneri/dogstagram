74b0810556dd4f3e737b0b06ed144805c77b5ad7
Jéssica Neri <jess.neri94@gmail.com>
2021-06-22 14:20:50 -0300

    Fix: Login style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,19 +5,22 @@ import LoginCreate from "./LoginCreate";
 import LoginNewPassword from "./LoginNewPassword";
 import LoginResetPassword from "./LoginResetPassword";
 import { UserContext } from "../../utils/userContext";
+import style from "./Login.module.css";
 
 export default function Login() {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="novasenha" element={<LoginNewPassword />} />
-        <Route path="resetar" element={<LoginResetPassword />} />
-      </Routes>
-    </div>
+    <section className={style.login}>
+      <div className={style.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="novasenha" element={<LoginNewPassword />} />
+          <Route path="resetar" element={<LoginResetPassword />} />
+        </Routes>
+      </div>
+    </section>
   );
 }
